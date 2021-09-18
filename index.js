c307ff92f1dc5f29c7691807f3a84af53ae94573
Ganeshkanchimad <86827594+Ganeshkanchimad@users.noreply.github.com>
2021-09-19 00:02:46 +0530

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
+var divOutput = document.querySelector("#div-output")
 
-function clickHandler() {
-    console.log("clicked")
-    console.log(txtInput.value)
-};
+
+
+var url = "https://api.funtranslations.com/translate/minion.json"
 
 btnTranslate.addEventListener("click", clickHandler);
+
+
+
+function clickHandler(event) {
+    console.log("clicked");
+    var input = txtInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            divOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error has occured!"))
+};
+
+function constructURL(Input) {
+    var encodedURI = encodeURI(Input);
+    return `${url}?text=${encodedURI}`;
+}
