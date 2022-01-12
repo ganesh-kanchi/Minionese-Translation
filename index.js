75c0a1fbbca45e87595fb6827e439d98947377e2
Ganesh Kanchi <ganeshkanchimad@gmail.com>
2022-01-12 20:12:24 +0530

    trim the code with ES6

--- index.js
+++ index.js
@@ -3,28 +3,17 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var divOutput = document.querySelector("#div-output")
 
-
-
 var url = "https://api.funtranslations.com/translate/minion.json"
 
-btnTranslate.addEventListener("click", clickHandler);
-
-
-
-function clickHandler(event) {
-    console.log("clicked");
+btnTranslate.addEventListener("click", () => {
     var input = txtInput.value;
     var finalURL = constructURL(input);
-    console.log(finalURL);
     fetch(finalURL)
         .then(response => response.json())
         .then(json => {
             divOutput.innerText = json.contents.translated;
         })
-        .catch(() => alert("some error has occured!"))
-};
+        .catch((error) => alert("some error has occured!", error))
+});
 
-function constructURL(Input) {
-    var encodedURI = encodeURI(Input);
-    return `${url}?text=${encodedURI}`;
-}
+const constructURL = Input => {`${url}?text=${encodeURI(Input)}`};
